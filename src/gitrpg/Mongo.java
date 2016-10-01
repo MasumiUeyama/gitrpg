6da1725b0fc1d25ea6aab70ac7ac5ed0da76f14b
MasumiUeyama <report.b13017@gmail.com>
2016-10-01 17:42:41 +0900

    dwr修正及び不要ファイル削除

--- src/gitrpg/Mongo.java
+++ src/gitrpg/Mongo.java
@@ -1,5 +1,65 @@
 package gitrpg;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+/**
+ * DWRでJSから呼ばれるメソッドはすべてpublicでなければならない．また，必要なクラスはすべてdwr.xmlに定義されている必要がある．
+ * @author Hiroshi
+ *
+ */
 public class Mongo {
-//test
+
+    /**
+     * 参考:http://d.hatena.ne.jp/Kazuhira/20131026/1382796711
+     * 正直これ見たかどうか覚えてないけどぐぐったらコレでた
+     * @return
+     * @throws Exception
+     */
+
+    public  String executeGet() {
+    	//文字列buffer作ったよ
+    	StringBuffer buffer = new StringBuffer();
+    	//しばらくjson見れたかどうかのチェック？
+        try {
+            URL url = new URL("https://api.github.com/repos/igakilab/api/commits");
+
+            HttpURLConnection connection = null;
+
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(),
+                                                                       StandardCharsets.UTF_8);
+                         BufferedReader reader = new BufferedReader(isr)) {
+                        //文末まで読んで全部引っ付ける
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            buffer.append(line);
+                        }
+                    }
+                }
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+
 }
