af8c46f854d29d9ae2bbf01498174fbdb56c1242
MasumiUeyama <report.b13017@gmail.com>
2016-10-12 21:22:12 +0900

    collectionを2つに分けました
    testに全て、test2に指定したcollectionぶち込み

--- src/gitrpg/Mongo.java
+++ src/gitrpg/Mongo.java
@@ -13,9 +13,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+
 /**
  * DWRでJSから呼ばれるメソッドはすべてpublicでなければならない．また，必要なクラスはすべてdwr.xmlに定義されている必要がある．
  * @author Hiroshi
@@ -30,9 +33,15 @@ public class Mongo {
 	}
 
     /**
-     * 参考:http://d.hatena.ne.jp/Kazuhira/20131026/1382796711
-     * https://translate.google.co.jp/translate?sl=en&tl=ja&js=y&prev=_t&hl=ja&ie=UTF-8&u=http%3A%2F%2Fmongodb.github.io%2Fmongo-java-driver%2F2.13%2Fgetting-started%2Fquick-tour%2F&edit-text=
-     * 正直これ見たかどうか覚えてないけどぐぐったらコレでた
+     * httpクライアント
+     * http://d.hatena.ne.jp/Kazuhira/20131026/1382796711
+     *
+     * Quick Tour
+     * http://mongodb.github.io/mongo-java-driver/3.3/driver/getting-started/quick-tour/
+     *
+     * json sample
+     * http://www.canetrash.jp/article/170576378.html
+
      * @return
      * @throws ParseException
      * @throws Exception
@@ -44,7 +53,7 @@ public class Mongo {
     	//しばらくjson見れたかどうかのチェック？
         try {
             //URL url = new URL("https://api.github.com/repos/igakilab/api/commits?page=3&per_page=50");
-            URL url = new URL("https://api.github.com/repos/igakilab/api/commits");
+            URL url = new URL("https://api.github.com/repos/masumiueyama/gitrpg/commits");
             HttpURLConnection connection = null;
 
             try {
@@ -76,6 +85,7 @@ public class Mongo {
         MongoDatabase database = mongoClient.getDatabase("mydb");
         //Document doc = Document.parse(reply);
         MongoCollection<Document> col = database.getCollection("test");
+        MongoCollection<Document> col2 = database.getCollection("test2");
         //col.insertOne(doc);
 
         JSONParser parser = new JSONParser();
@@ -83,28 +93,75 @@ public class Mongo {
 
         Object obj = parser.parse(reply);
         JSONArray json = (JSONArray)obj;
+
+        //リセット
         col.deleteMany(new Document());
+        col2.deleteMany(new Document());
+
         for(Object obj0 : json){
         	JSONObject tmp = (JSONObject)obj0;
         	Document doc1 = Document.parse(tmp.toJSONString());
-        	doc1.append("msg", "井垣先生、無条件で単位をください");
         	col.insertOne(doc1);
         }
 
 
-        Document doc3 = new Document("commit.author.date", "2016-08-02T09:27:33Z");
-        //doc3.append("commit", new Document("author", new Document("date", "2016-08-02T09:27:33Z")));
-        col.deleteMany(doc3);
+/**
+        for(int i=0;i<=count;i++){
+        	Document doc3 = new Document("commit.author.name", "MasumiUeyama");
+        	//col.deleteMany(doc3);
+        	//DeleteResult deleteResult = col.deleteMany(doc3); //System.out.println( "でりーと" +deleteResult.getDeletedCount());
+        	//System.out.println(deleteResult);
+        }
+**/
 
-        //ちゃんと動くやつ
-        //col.deleteMany(new Document("sha", "01641de3f3b18b2f307c9067d68faba122fe4953"));
 
         long count = col.count();
-        System.out.println(count + "件やでえええええええcaえa");
+        System.out.println("log:a" + count);
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("commit.author.name", "ue");
+		System.out.println(query);
+		Document doc4;
+
+		MongoCursor<Document> cursor = col.find(query).iterator();
+		while (cursor.hasNext()) {
+			doc4 = cursor.next();
+	       	col2.insertOne(doc4);
+			//col2.insertOne(cursor.next());
+		}
+
+        count = col.count();
+        System.out.println("log:b" + count);
+
+        /**
+		MongoCursor<Document> cursor = col.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+
+
+        count = col.count();
+
+        for(int i=0;i<=count;i++){
+        	Document doc3 = new Document("commit.author.name", "ue");
+        	col.deleteMany(doc3);
+        }
+
+
+        ちゃんと動くやつ
+        col.deleteMany(new Document("sha", "01641de3f3b18b2f307c9067d68faba122fe4953"));
+        動かなe
+    	doc3.append("commit", new Document("author", new Document("date", "2016-08-02T09:27:33Z")));
+        **/
+
+        count = col.count();
+        System.out.println("log:c:" + count);
         mongoClient.close();
 
-        //col.deleteMany(new Document("sha", "01641de3f3b18b2f307c9067d68faba122fe4953"));
-        
         //ctrl+alt+x -> j
 
 
